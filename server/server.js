6d900417f7c8024f1820d65323027b3d4c60046a
saimsaeed <saimsaeed640@gmai.com>
2024-11-22 16:02:02 +0500

    Updated

--- server/server.js
+++ server/server.js
@@ -51,7 +51,6 @@ app.use(express.static(path.join(__dirname, "..", "client", "build")));
 app.get("*",(req,res)=>{
 //  res.sendFile(path.resolve(__dirname,"client","build","index.html"))
 res.sendFile(path.resolve(__dirname, "..", "client", "build", "index.html"));
-
 })
 }else{
     app.get("/",(req,res)=>{
